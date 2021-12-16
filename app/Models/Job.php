4683c998617c10792e6c12b620d9e5288d55906a
sandeeppal270 <sandeeppal270@gmail.com>
2021-12-16 16:40:35 +0530

    commit

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Job extends Model
 {
@@ -19,6 +20,9 @@ class Job extends Model
     }
     public function users(){
         return $this->belongsToMany(User::class)->withTimestamps();
+    }
+    public function checkApplication(){
+        DB::table('job_user')->where('user_id',auth()->user()->id)->where('job_id',$this->id)->exists();
     }
 
 
